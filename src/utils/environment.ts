11983ff22ec23b680187fb3328cbc9b3f9a5984b
AnhMTV <maithachvietanh@gmail.com>
2023-07-17 20:22:48 +0700

    Update deployment config for dotinvietnam-dev

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -8,7 +8,7 @@ export interface EnvironmentType {
 }
 export const ENVIRONMENT : EnvironmentType = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  RMRK_COLLECTION_ID: process.env.RMRK_COLLECTION_ID || '80971274db0b27d618-SWLBLD',
+  RMRK_COLLECTION_ID: process.env.RMRK_COLLECTION_ID || '80971274db0b27d618-SWEKXV',
   INSTRUCTION_URL: process.env.INSTRUCTION_URL || 'https://youtu.be/7O2XUmAPpKE',
   API_ENDPOINT: process.env.API_ENDPOINT || 'https://nft-minting.subwallet.app',
 }
