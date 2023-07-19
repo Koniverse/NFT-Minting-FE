e8d882514d98a78e785dc56a472d0d983dc3e4e2
AnhMTV <maithachvietanh@gmail.com>
2023-07-19 21:42:07 +0700

    Update open in browser with deeplink

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,5 +1,7 @@
 // Load all environment variables from .env file
 
+import MobileDetect from "mobile-detect";
+
 export interface EnvironmentType {
   NODE_ENV: string;
   INSTRUCTION_URL: string;
@@ -12,3 +14,8 @@ export const ENVIRONMENT : EnvironmentType = {
   INSTRUCTION_URL: process.env.INSTRUCTION_URL || 'https://youtu.be/7O2XUmAPpKE',
   API_ENDPOINT: process.env.API_ENDPOINT || 'https://nft-minting-demo01.subwallet.app',
 }
+
+const detect = new MobileDetect(navigator.userAgent, 1200);
+export const isAndroid = detect.os() === 'AndroidOS'
+export const isIOS = detect.os() === 'iOS'
+export const isMobile = isIOS || isAndroid;
