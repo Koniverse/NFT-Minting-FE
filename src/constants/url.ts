aa1f5bb91fea0d795518221b7907f3fa8e2b42cf
AnhMTV <maithachvietanh@gmail.com>
2023-07-19 16:18:41 +0700

    Update to fix preview issue on twitter

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -3,6 +3,6 @@ export const TELEGRAM_URL = 'https://t.me/subwallet';
 export const TWITTER_URL = 'https://twitter.com/subwalletapp';
 export const DISCORD_URL = 'https://discord.com/invite/vPCN4vdB8v';
 export const DOWNLOAD_URL = 'https://www.subwallet.app/download.html';
-export const SHARE_URL = 'https://nft.dotinvietnam.com/';
+export const SHARE_URL = 'https://nft.dotinvietnam.com';
 export const SHARE_TEXT = '🎉 Woohoo! Just minted my very own @Polkadot Power Passport for the epic @PolkadotDecoded 2023 event series! 🚀✨ Easy free mint with real utilities, you don’t want to miss it 👉';
 export const SHARE_TAGS=['PolkadotDecoded', 'VietnamSatelliteEvent', 'OnlyOnPolkadot']
